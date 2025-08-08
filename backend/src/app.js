f0a5e92d229947e13c2c0645b2b9679ee7445b11
Vaishnavi Pawar <vaishnavi.22211188@viit.ac.in>
2025-08-08 15:44:20 +0530

    task model creation

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,6 @@ app.use(cookieParser())
 
 import userRoutes from "./routes/user.routes.js";
 
-app.use("/api/v1/user", userRoutes);
+app.use("/api/v1", userRoutes);
 
 export {app};
---
+++ backend/src/models/task.model.js
@@ -0,0 +1,46 @@
+import mongoose, {Schema} from "mongoose";
+
+const taskSchema = new Schema(
+    {
+        user_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        task: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ['pending', 'completed'] 
+        },
+        collaborators: {
+            type: [String],
+            default: [],
+            validate: {
+                validator: function(arr) {
+                    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                    const invalidEmails = arr.filter(email => !emailRegex.test(email));
+                    return invalidEmails.length === 0;
+                },
+                message: "one or more collaborator emails are invalid"
+            }
+        },
+        deadline: {
+            type: Date,
+            required: true,
+            validate: {
+                validator: function (value) {
+                    return value > new Date();
+                },
+                message: "Deadline must be a future date."
+            }
+        }
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+export const Task = mongoose.model("Task", taskSchema);
